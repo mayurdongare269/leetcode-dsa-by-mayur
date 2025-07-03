9885dc1d3198e04091eb21be001bf5a404c195b1
Mayur Dongare <144617577+mayurdongare269@users.noreply.github.com>
2025-07-03 15:48:23 +0530

    Time: 8 ms (84.70%) | Memory: 44.6 MB (6.04%) - LeetSync

--- 326-power-of-three/power-of-three.java
+++ 326-power-of-three/power-of-three.java
@@ -5,7 +5,9 @@ class Solution {
         while(n % 3 == 0) {
             n /= 3;
         }
-
         return n == 1;
+
+        // 1162261467 is 3^19, the largest power of 3 in 32-bit int
+        // return n > 0 && 1162261467 % n == 0;
     }
 }
