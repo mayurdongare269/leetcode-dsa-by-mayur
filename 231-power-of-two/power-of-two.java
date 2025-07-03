69d2fc1b257f77da19e4bbd6569e18d64e39a12f
Mayur Dongare <144617577+mayurdongare269@users.noreply.github.com>
2025-07-03 15:41:55 +0530

    Time: 0 ms (100.00%) | Memory: 41.5 MB (5.07%) - LeetSync

---
+++ 231-power-of-two/power-of-two.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean isPowerOfTwo(int n) {
+        // if(n == 1) return true;
+        // return n % 2 == 0;
+
+        if (n <= 0) return false;
+        while (n % 2 == 0) {
+            n /= 2;
+        }
+        return n == 1;
+    }
+}
