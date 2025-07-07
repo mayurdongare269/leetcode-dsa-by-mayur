82f9eca1f1a3f47e171c440c68ace7393c7eaeff
Mayur Dongare <144617577+mayurdongare269@users.noreply.github.com>
2025-07-07 18:05:52 +0530

    Time: 5 ms (99.47%) | Memory: 45.4 MB (24.34%) - LeetSync

---
+++ 2469-longest-subsequence-with-limited-sum/longest-subsequence-with-limited-sum.java
@@ -0,0 +1,41 @@
+import java.util.*;
+
+class Solution {
+    public int[] answerQueries(int[] nums, int[] queries) {
+        Arrays.sort(nums);
+        int n = nums.length;
+        
+        // Build prefix sums
+        int[] prefix = new int[n];
+        prefix[0] = nums[0];
+        for (int i = 1; i < n; i++) {
+            prefix[i] = prefix[i - 1] + nums[i];
+        }
+        
+        int m = queries.length;
+        int[] ans = new int[m];
+        
+        // Answer each query with binary search on prefix sums
+        for (int i = 0; i < m; i++) {
+            int q = queries[i];
+            int idx = upperBound(prefix, q);
+            ans[i] = idx; // idx gives count of elements ≤ q
+        }
+        return ans;
+    }
+    
+    // Finds the first index i such that prefix[i] > target,
+    // which is effectively the number of elements ≤ target.
+    private int upperBound(int[] prefix, int target) {
+        int lo = 0, hi = prefix.length; // hi is exclusive
+        while (lo < hi) {
+            int mid = lo + (hi - lo) / 2;
+            if (prefix[mid] <= target) {
+                lo = mid + 1;
+            } else {
+                hi = mid;
+            }
+        }
+        return lo;
+    }
+}
