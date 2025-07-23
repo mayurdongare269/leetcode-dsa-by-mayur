f8268ab2142d0f55d85b7c6f9ebd3db429059023
Mayur Dongare <144617577+mayurdongare269@users.noreply.github.com>
2025-07-23 22:06:07 +0530

    Time: 885 ms (6.47%) | Memory: 41.8 MB (49.56%) - LeetSync

---
+++ 647-palindromic-substrings/palindromic-substrings.java
@@ -0,0 +1,28 @@
+class Solution {
+
+    private boolean check(String s, int i, int j) {
+        if(i > j) {
+            return true;
+        }
+
+        if(s.charAt(i) == s.charAt(j)) {
+            return check(s, i+1, j-1);
+        }
+
+        return false;
+    }
+    public int countSubstrings(String s) {
+        int count = 0;
+        int n = s.length();
+
+        for(int i = 0; i < n; i++) {
+            for(int j = i; j < n; j++) {
+                if(check(s, i, j)) {
+                    count++;
+                }
+            }
+        }
+
+        return count;
+    }
+}
