bb655ffddc56b0d18461bd2befc5d29e0db74d08
Mayur Dongare <144617577+mayurdongare269@users.noreply.github.com>
2025-08-09 13:56:19 +0530

    Time: 0 ms (100.00%) | Memory: 41.8 MB (7.71%) - LeetSync

---
+++ 410-split-array-largest-sum/split-array-largest-sum.java
@@ -0,0 +1,48 @@
+class Solution { // similar to Book Allocation Problem and Painter's partition
+
+    public int splitArray(int[] nums, int k) {
+        return findPages(nums, k);
+    }
+
+
+    private int countStud(int arr[], int pages) {
+        int n = arr.length; 
+        int std = 1, pagesStu = 0;
+        
+        for(int i = 0; i < n; i++) {
+            if(pagesStu + arr[i] <= pages) {
+                pagesStu += arr[i];
+            } else {
+                std += 1;
+                pagesStu = arr[i];
+            }
+        } 
+        return std;
+    }
+    
+    public int findPages(int[] arr, int k) {
+        // code here
+        int n = arr.length;
+        if(k > n) return -1;
+        
+        int low = Integer.MIN_VALUE;
+        int high = 0;
+        
+        for(int i = 0; i < n; i++) {
+            low = Math.max(low, arr[i]);
+            high += arr[i];
+        }
+        
+        while(low <= high) {
+            int mid = low + (high - low) / 2;
+            
+            int noOfStud = countStud(arr, mid);
+            if(noOfStud > k) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return low;
+    }
+}
