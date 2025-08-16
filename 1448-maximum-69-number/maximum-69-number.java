6ed500f1b29d5739f5550f245b91059c44321bf8
Mayur Dongare <144617577+mayurdongare269@users.noreply.github.com>
2025-08-16 12:28:33 +0530

    Time: 1 ms (35.60%) | Memory: 40.5 MB (72.77%) - LeetSync

---
+++ 1448-maximum-69-number/maximum-69-number.java
@@ -0,0 +1,33 @@
+class Solution { // easy and learnable que .. good one
+    private int[] convertNumToArr(int num) {
+        String numStr = String.valueOf(num); // convert num to string
+        char[] charArr = numStr.toCharArray(); // convert to charArr
+        int[] digits = new int[charArr.length];// Create int array for digits
+
+        for(int i = 0; i < charArr.length; i++) {
+            digits[i] = Character.getNumericValue(charArr[i]); //Convert char to int
+        }
+
+        return digits;
+    }
+
+    public int maximum69Number (int num) {
+        int[] digits = convertNumToArr(num);
+
+        // Change the first 6 to 9
+        for(int i = 0; i < digits.length; i++) {
+            if(digits[i] == 6) {
+                digits[i] = 9;
+                break;
+            }
+        }
+
+        // Convert array back to number
+        StringBuilder sb = new StringBuilder();
+        for(int d : digits) {
+            sb.append(d);
+        }
+
+        return Integer.parseInt(sb.toString());
+    }
+}
