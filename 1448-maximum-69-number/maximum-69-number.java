685b3bc909092b7898cb913f8e554093ee2acb38
Mayur Dongare <144617577+mayurdongare269@users.noreply.github.com>
2025-08-16 12:30:31 +0530

    Time: 2 ms (17.88%) | Memory: 41.3 MB (5.83%) - LeetSync

--- 1448-maximum-69-number/maximum-69-number.java
+++ 1448-maximum-69-number/maximum-69-number.java
@@ -1,33 +1,43 @@
 class Solution { // easy and learnable que .. good one
-    private int[] convertNumToArr(int num) {
-        String numStr = String.valueOf(num); // convert num to string
-        char[] charArr = numStr.toCharArray(); // convert to charArr
-        int[] digits = new int[charArr.length];// Create int array for digits
+    // Methond - 1(long)
 
-        for(int i = 0; i < charArr.length; i++) {
-            digits[i] = Character.getNumericValue(charArr[i]); //Convert char to int
-        }
+    // private int[] convertNumToArr(int num) {
+    //     String numStr = String.valueOf(num); // convert num to string
+    //     char[] charArr = numStr.toCharArray(); // convert to charArr
+    //     int[] digits = new int[charArr.length];// Create int array for digits
 
-        return digits;
-    }
+    //     for(int i = 0; i < charArr.length; i++) {
+    //         digits[i] = Character.getNumericValue(charArr[i]); //Convert char to int
+    //     }
 
+    //     return digits;
+    // }
+
+    // public int maximum69Number (int num) {
+    //     int[] digits = convertNumToArr(num);
+
+    //     // Change the first 6 to 9
+    //     for(int i = 0; i < digits.length; i++) {
+    //         if(digits[i] == 6) {
+    //             digits[i] = 9;
+    //             break;
+    //         }
+    //     }
+
+    //     // Convert array back to number
+    //     StringBuilder sb = new StringBuilder();
+    //     for(int d : digits) {
+    //         sb.append(d);
+    //     }
+
+    //     return Integer.parseInt(sb.toString());
+    // }
+
+    // Mathod- 2
     public int maximum69Number (int num) {
-        int[] digits = convertNumToArr(num);
-
-        // Change the first 6 to 9
-        for(int i = 0; i < digits.length; i++) {
-            if(digits[i] == 6) {
-                digits[i] = 9;
-                break;
-            }
-        }
-
-        // Convert array back to number
-        StringBuilder sb = new StringBuilder();
-        for(int d : digits) {
-            sb.append(d);
-        }
-
-        return Integer.parseInt(sb.toString());
+        String numStr = String.valueOf(num);
+        // replace only the first '6' with '9'
+        String result = numStr.replaceFirst("6", "9");
+        return Integer.parseInt(result);
     }
 }
