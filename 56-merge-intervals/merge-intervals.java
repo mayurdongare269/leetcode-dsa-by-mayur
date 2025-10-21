35b3f3a3e4c24ba2300dcbe495d05363fa55d31c
Mayur Dongare <144617577+mayurdongare269@users.noreply.github.com>
2025-10-21 10:58:22 +0530

    Time: 8 ms (84.93%) | Memory: 46.6 MB (73.75%) - LeetSync

---
+++ 56-merge-intervals/merge-intervals.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int[][] merge(int[][] intervals) {
+        if(intervals == null || intervals.length == 0) {
+            return new int[0][0];
+        }
+
+        //1. sort by begininh
+        Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
+
+        //2. list to store
+        List<int[]> merged = new ArrayList<>();
+
+        //3. final logic through iteration
+        for(int[] interval : intervals) {
+            // case1: non overlapped
+            if(merged.isEmpty() || merged.get(merged.size() - 1)[1] < interval[0]) {
+                merged.add(interval);
+            } else { // c2: overlapping -> merge it
+                merged.get(merged.size() - 1)[1] = Math.max(merged.get(merged.size() - 1)[1], interval[1]);
+            }
+        }
+
+        //4. convert list to arr
+        return merged.toArray(new int[merged.size()][]);
+
+    }
+}
