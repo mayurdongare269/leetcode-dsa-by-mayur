49ed210fb0ea346c27f2d74695916d7cf8e34d26
Mayur Dongare <144617577+mayurdongare269@users.noreply.github.com>
2025-10-24 17:03:43 +0530

    Time: 100 ms (79.27%) | Memory: 46.1 MB (8.55%) - LeetSync

---
+++ 732-my-calendar-iii/my-calendar-iii.java
@@ -0,0 +1,27 @@
+class MyCalendarThree {
+
+     private TreeMap<Integer, Integer> timeline;
+    
+    public MyCalendarThree() {
+        timeline = new TreeMap<>();
+    }
+    
+    public int book(int start, int end) {
+        timeline.put(start, timeline.getOrDefault(start, 0) + 1);
+        timeline.put(end, timeline.getOrDefault(end, 0) - 1);
+
+        int active = 0, maxOverlap = 0;
+        for (int val : timeline.values()) {
+            active += val;
+            maxOverlap = Math.max(maxOverlap, active);
+        }
+
+        return maxOverlap;
+    }
+}
+
+/**
+ * Your MyCalendarThree object will be instantiated and called as such:
+ * MyCalendarThree obj = new MyCalendarThree();
+ * int param_1 = obj.book(startTime,endTime);
+ */
