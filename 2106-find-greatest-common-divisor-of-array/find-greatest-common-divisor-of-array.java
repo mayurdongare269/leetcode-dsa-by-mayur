9599cda1db051659b0d013695443cb808ca2696b
Mayur Dongare <144617577+mayurdongare269@users.noreply.github.com>
2025-10-30 22:35:52 +0530

    Time: 0 ms (100.00%) | Memory: 43.1 MB (84.06%) - LeetSync

--- 2106-find-greatest-common-divisor-of-array/find-greatest-common-divisor-of-array.java
+++ 2106-find-greatest-common-divisor-of-array/find-greatest-common-divisor-of-array.java
@@ -20,13 +20,13 @@ class Solution {
 
     //// Euclidean Algorithm
     private int gcd(int a, int b) {
-        // if(b == 0) return a;
-        // return gcd(b, a % b);
-        while(b!=0) {
-            int temp = b;
-            b = a % b;
-            a = temp;
-        }
-        return a;
+        if(b == 0) return a;
+        return gcd(b, a % b);
+        // while(b!=0) {
+        //     int temp = b;
+        //     b = a % b;
+        //     a = temp;
+        // }
+        // return a;
     }
 }
