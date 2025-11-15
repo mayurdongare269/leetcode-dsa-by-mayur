229e8053be12efe02558f771910814968fe2dcc3
Mayur Dongare <144617577+mayurdongare269@users.noreply.github.com>
2025-11-15 19:52:35 +0530

    Time: 841 ms (72.90%) | Memory: 46.4 MB (29.91%) - LeetSync

---
+++ 3479-count-the-number-of-substrings-with-dominant-ones/count-the-number-of-substrings-with-dominant-ones.java
@@ -0,0 +1,27 @@
+class Solution {
+  public int numberOfSubstrings(String s) {
+    int ans = 0;
+
+
+    for (int zero = 0; zero + zero * zero <= s.length(); ++zero) {
+      int lastInvalidPos = -1;
+      int[] count = new int[2];
+      for (int l = 0, r = 0; r < s.length(); ++r) {
+        ++count[s.charAt(r) - '0'];
+        for (; l < r; ++l)
+          if (s.charAt(l) == '0' && count[0] > zero) {
+            --count[0]; 
+            lastInvalidPos = l;
+          } else if (s.charAt(l) == '1' && count[1] - 1 >= zero * zero) {
+            --count[1]; 
+          } else {
+            break; 
+          }
+        if (count[0] == zero && count[1] >= zero * zero)
+          ans += l - lastInvalidPos;
+      }
+    }
+
+    return ans;
+  }
+}
