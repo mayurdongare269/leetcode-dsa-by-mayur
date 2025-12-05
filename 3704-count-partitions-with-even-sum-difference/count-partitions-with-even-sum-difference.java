ea405213141184e5ece51d258d219278beef0698
Mayur Dongare <144617577+mayurdongare269@users.noreply.github.com>
2025-12-05 12:32:22 +0530

    Time: 0 ms (100.00%) | Memory: 43.7 MB (24.46%) - LeetSync

--- 3704-count-partitions-with-even-sum-difference/count-partitions-with-even-sum-difference.java
+++ 3704-count-partitions-with-even-sum-difference/count-partitions-with-even-sum-difference.java
@@ -1,23 +1,34 @@
-class Solution {// done ,.... two pass solution ...
+class Solution {// done ,.... 
     public int countPartitions(int[] nums) {
-        int count = 0;
+        // one pass solution(need some maths...)
         int n = nums.length;
+        int sum = sumf(nums, 0, n-1);
+        if(sum % 2 == 0) {
+            return n - 1;
+        }
+        return 0;
+
+
+
+        // // two pass solution ...
+        // int count = 0;
+        // int n = nums.length;
         
-        int totSum = 0;
-        for(int i = 0; i < n; i++) {
-            totSum += nums[i];
-        }
+        // int totSum = 0;
+        // for(int i = 0; i < n; i++) {
+        //     totSum += nums[i];
+        // }
 
-        for(int i = 0; i <= n-2; i++) {
-            int prev = sumf(nums, 0, i);
-            int next = totSum - prev;
-            int ans = prev - next;
-            if(ans % 2 == 0) {
-                count++;
-            }
-        }
+        // for(int i = 0; i <= n-2; i++) {
+        //     int prev = sumf(nums, 0, i);
+        //     int next = totSum - prev;
+        //     int ans = prev - next;
+        //     if(ans % 2 == 0) {
+        //         count++;
+        //     }
+        // }
 
-        return count;
+        // return count;
     }
 
     public int sumf(int[] arr, int s, int e) {
