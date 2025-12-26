c2ff7441befd49830140bcdc72542fad835fabd0
Mayur Dongare <144617577+mayurdongare269@users.noreply.github.com>
2025-12-26 19:27:16 +0530

    Time: 11 ms (75.86%) | Memory: 46.5 MB (85.52%) - LeetSync

---
+++ 2576-minimum-penalty-for-a-shop/minimum-penalty-for-a-shop.java
@@ -0,0 +1,29 @@
+class Solution { //ntslv
+    public int bestClosingTime(String customers) {
+        int penalty = 0;
+
+        // Initial penalty: shop closed all day
+        for (char c : customers.toCharArray()) {
+            if (c == 'Y') penalty++;
+        }
+
+        int minPenalty = penalty;
+        int bestHour = 0;
+
+        // Move closing time from 1 to n
+        for (int i = 0; i < customers.length(); i++) {
+            if (customers.charAt(i) == 'Y') {
+                penalty--; 
+            } else {
+                penalty++; 
+            }
+
+            if (penalty < minPenalty) {
+                minPenalty = penalty;
+                bestHour = i + 1;
+            }
+        }
+
+        return bestHour;
+    }
+}
